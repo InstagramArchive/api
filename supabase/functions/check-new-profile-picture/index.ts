fabb001140383094564ca46f1b9d715e8ae2d403
qypol342 <qypol342@gmail.com>
2024-05-05 15:05:13 +0200

    classic update

--- supabase/functions/check-new-profile-picture/index.ts
+++ supabase/functions/check-new-profile-picture/index.ts
@@ -12,20 +12,14 @@ Deno.serve(async (req) => {
     // Create a Supabase client with the Auth context of the logged in user.
     const supabaseClient = createClient(
       // Supabase API URL - env var exported by default.
-      Deno.env.get("SUPABASE_URL") ?? "",
+      Deno.env.get('SUPABASE_URL') ?? '',
       // Supabase API ANON KEY - env var exported by default.
-      Deno.env.get("SUPABASE_SERVICE_ROLE_KEY") ?? "",
+      Deno.env.get('SUPABASE_SERVICE_ROLE_KEY') ?? '',
       // Create client with Auth context of the user that called the function.
       // This way your row-level-security (RLS) policies are applied.
-      {
-        global: {
-          headers: { Authorization: req.headers.get("Authorization")! },
-        },
-      },
+      //{ global: { headers: { Authorization: req.headers.get('Authorization')! } } }
     );
-    //[ { name: "carla.tstr" }, { name: "elisepgn" } ]
     const userList = await getInstagramProfileName(supabaseClient);
-
     for (const user of userList) {
       const userData = await getUserData(user.name);
       console.log(userData);
